2706fa1db226ff51b5a07b8b6af785c436c397b9
colorlessenergy <theluckorange@yahoo.com>
2018-01-30 19:40:10 -0800

    Change background property to backgroundColor to make it work on mozilla

--- colorGame.js
+++ colorGame.js
@@ -29,12 +29,12 @@ function setUpModeButtons () {
       modeButtons[1].classList.remove("selected");
       this.classList.add("selected");
       // ternary operator
-      this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-      // if (this.textContent === "Easy") {
-      //   numSquares = 3;
-      // } else {
-      //   numSquares = 6;
-      // }
+      // this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+      if (this.textContent === "Easy") {
+        numSquares = 3;
+      } else {
+        numSquares = 6;
+      }
       reset();
     });
   }
@@ -45,15 +45,15 @@ function setUpSquares () {
     //add click listeners to squares
     squares[i].addEventListener("click", function () {
       // grab color of clicked square
-       var clickedColor = this.style.background;
+       var clickedColor = this.style.backgroundColor;
       // compare color to pickedcolor
       if (clickedColor === pickedColor) {
         resetButton.textContent = "Play Again?"
         messageDisplay.textContent = "Correct!";
         changeColors(clickedColor);
-        h1.style.background = clickedColor;
+        h1.style.backgroundColor = clickedColor;
       } else {
-        this.style.background = "#232323";
+        this.style.backgroundColor = "#232323";
         messageDisplay.textContent = "Try Again";
       }
     });
@@ -71,12 +71,12 @@ function reset () {
   for (var i = 0; i < squares.length; i++) {
     if (colors[i]) {
       squares[i].style.display = "block";
-      squares[i].style.background = colors[i];
+      squares[i].style.backgroundColor = colors[i];
     } else {
       squares[i].style.display = "none";
     }
   }
-  h1.style.background = "steelblue";
+  h1.style.backgroundColor = "steelblue";
   resetButton.textContent = "New Colors";
   messageDisplay.textContent = "";
 }
@@ -91,7 +91,7 @@ function changeColors (color) {
   // loops through all the squares
   for (var i = 0; i < squares.length; i++) {
     // change each color to match the given color
-    squares[i].style.background = color;
+    squares[i].style.backgroundColor = color;
   }
 }
 
